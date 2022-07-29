19705918184f35dd296d57830007e5e5958a6b12
aedenthorn <aedenthorn1@gmail.com>
2022-07-28 20:58:07 -0400

    etc

--- CameraTweaks/BepInExPlugin.cs
+++ CameraTweaks/BepInExPlugin.cs
@@ -3,20 +3,14 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using Cinemachine;
 using HarmonyLib;
-using Pathea;
-using Pathea.FrameworkNs;
-using Pathea.SleepNs;
-using Pathea.TimeNs;
-using Pathea.UISystemV2.UI;
-using Pathea.UISystemV2.UIControl;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using static Cinemachine.CinemachineFreeLook;
 
 namespace CameraTweaks
 {
-    [BepInPlugin("aedenthorn.CameraTweaks", "CameraTweaks", "0.1.0")]
+    [BepInPlugin("aedenthorn.CameraTweaks", "CameraTweaks", "0.2.0")]
     public partial class BepInExPlugin : BaseUnityPlugin
     {
         private static BepInExPlugin context;
@@ -26,6 +20,13 @@ namespace CameraTweaks
         public static ConfigEntry<float> zoomSpeed;
         public static ConfigEntry<string> increaseDistanceKey;
         public static ConfigEntry<string> decreaseDistanceKey;
+        public static ConfigEntry<string> increaseHeightKey;
+        public static ConfigEntry<string> decreaseHeightKey;
+        public static ConfigEntry<string> increaseRadiusKey;
+        public static ConfigEntry<string> decreaseRadiusKey;
+        public static ConfigEntry<string> resetCameraKey;
+
+        public static Dictionary<int, List<Orbit>> cameraDict = new Dictionary<int, List<Orbit>>();
 
         public static void Dbgl(string str = "", LogLevel logLevel = LogLevel.Debug)
         {
@@ -39,8 +40,13 @@ namespace CameraTweaks
             modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
             isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
             zoomSpeed = Config.Bind<float>("Options", "ZoomSpeed", 0.01f, "Speed to zoom in and out");
+            resetCameraKey = Config.Bind<string>("Options", "ResetCameraKey", "[5]", "Key to reset camera to default");
             increaseDistanceKey = Config.Bind<string>("Options", "IncreaseDistanceKey", "[2]", "Key to hold to increase distance");
             decreaseDistanceKey = Config.Bind<string>("Options", "DecreaseDistanceKey", "[8]", "Key to hold to decrease distance");
+            increaseHeightKey = Config.Bind<string>("Options", "IncreaseHeightKey", "[9]", "Key to hold to increase height");
+            decreaseHeightKey = Config.Bind<string>("Options", "DecreaseHeightKey", "[3]", "Key to hold to decrease height");
+            increaseRadiusKey = Config.Bind<string>("Options", "IncreaseRadiusKey", "[1]", "Key to hold to increase radius");
+            decreaseRadiusKey = Config.Bind<string>("Options", "DecreaseRadiusKey", "[7]", "Key to hold to decrease radius");
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 
@@ -52,13 +58,56 @@ namespace CameraTweaks
         {
             public static void Postfix(CinemachineFreeLook __instance)
             {
-                float zoomPercent = 1;
-                if (AedenthornUtils.CheckKeyHeld(increaseDistanceKey.Value))
-                    zoomPercent += zoomSpeed.Value;
-                else if (AedenthornUtils.CheckKeyHeld(decreaseDistanceKey.Value))
-                    zoomPercent -= zoomSpeed.Value;
-                else return;
+                if (!cameraDict.TryGetValue(__instance.GetInstanceID(), out List<Orbit> origOrbits))
+                {
+                    Dbgl("Recording default orbits for camera");
+                    origOrbits = new List<Orbit>();
+                    foreach(Orbit orbit in __instance.m_Orbits)
+                    {
+                        origOrbits.Add(orbit);
+                    }
+                    cameraDict[__instance.GetInstanceID()] = origOrbits;
+                }
 
+                float heightPercent = 1;
+                float radiusPercent = 1;
+                if (AedenthornUtils.CheckKeyHeld(increaseDistanceKey.Value))
+                {
+                    radiusPercent += zoomSpeed.Value;
+                    heightPercent += zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyHeld(decreaseDistanceKey.Value))
+                {
+                    radiusPercent -= zoomSpeed.Value;
+                    heightPercent -= zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyHeld(increaseHeightKey.Value))
+                {
+                    heightPercent += zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyHeld(decreaseHeightKey.Value))
+                {
+                    heightPercent -= zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyHeld(increaseRadiusKey.Value))
+                {
+                    radiusPercent += zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyHeld(decreaseRadiusKey.Value))
+                {
+                    radiusPercent -= zoomSpeed.Value;
+                }
+                else if (AedenthornUtils.CheckKeyDown(resetCameraKey.Value))
+                {
+                    Dbgl("Resetting camera");
+                    for (int i = 0; i < __instance.m_Orbits.Length; i++)
+                    {
+                        __instance.m_Orbits[i].m_Height = origOrbits[i].m_Height;
+                        __instance.m_Orbits[i].m_Radius = origOrbits[i].m_Radius;
+                    }
+                    return;
+                }
+                else return;
 
                 var originalOrbits = new CinemachineFreeLook.Orbit[__instance.m_Orbits.Length];
                 for (int i = 0; i < __instance.m_Orbits.Length; i++)
@@ -68,8 +117,8 @@ namespace CameraTweaks
                 }
                 for (int i = 0; i < __instance.m_Orbits.Length; i++)
                 {
-                    __instance.m_Orbits[i].m_Height = Mathf.Max(0.02f, originalOrbits[i].m_Height * zoomPercent);
-                    __instance.m_Orbits[i].m_Radius = Mathf.Max(0.02f, originalOrbits[i].m_Radius * zoomPercent);
+                    __instance.m_Orbits[i].m_Height = Mathf.Max(0.02f, originalOrbits[i].m_Height * heightPercent);
+                    __instance.m_Orbits[i].m_Radius = Mathf.Max(0.02f, originalOrbits[i].m_Radius * radiusPercent);
                 }
             }
         }
